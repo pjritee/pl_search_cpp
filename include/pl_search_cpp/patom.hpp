abac6f485c52b57044289724446f9d6b6f83ea5c
pjr <pjr@itee.uq.edu.au>
2025-02-05 09:13:01 +1000

    restructure directories

---
+++ include/pl_search_cpp/patom.hpp
@@ -0,0 +1,69 @@
+/*
+MIT License
+
+Copyright (c) 2025 [Peter Robinson]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#ifndef PATOM_HPP
+#define PATOM_HPP
+
+#include "term.hpp"
+#include <string>
+#include <typeinfo>
+
+
+// PAtom objects approximate Prolog atoms
+class PAtom : public Term {
+public:
+    PAtom(const std::string& name) : name(name) {}
+
+    Term* dereference() override {
+        return this;
+    }
+
+    bool bind(Term* t) override {
+        return false; // Atoms cannot be bound to other terms
+    }
+
+    void reset(Term* t) override {
+        // No-op for PAtom
+    }
+
+    std::string getName() const {
+        return name;
+    }
+
+    std::string repr() const override {
+       return name;
+    }
+
+    bool isEqualTo(Term& t) override {
+        PAtom* a = dynamic_cast<PAtom*>(&t);
+        if (a == nullptr) return false;
+        return name == a->name;
+    }
+
+    bool isLessThan(Term& t) override;
+
+private:
+    std::string name;
+};
+
+#endif // PATOM_HPP
