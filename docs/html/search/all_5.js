e0143d3b2055cfbe7276a8c4f81de96b8f88a340
pjr <pjr@itee.uq.edu.au>
2026-02-25 11:44:48 +1000

    update docs

--- docs/html/search/all_5.js
+++ docs/html/search/all_5.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['elements_0',['elements',['../classpl__search_1_1CList.html#a7d5df37ecf162c9550740e5ab3cdb6bb',1,'pl_search::CList']]],
   ['else_5fpred_1',['else_pred',['../classpl__search_1_1IfThenElse.html#af836552a7d6c881a43c1201596055d9c',1,'pl_search::IfThenElse']]],
-  ['engine_2',['engine',['../classpl__search_1_1Engine.html',1,'pl_search::Engine'],['../classpl__search_1_1Engine.html#a961cef69a59a6c81ff8e5965ba74ebfa',1,'pl_search::Engine::Engine()'],['../classpl__search_1_1VarChoiceIterator.html#af557b6f1a2177f6ec6edebab0d7be2f7',1,'pl_search::VarChoiceIterator::engine'],['../classpl__search_1_1Pred.html#a3866f81bf27c42b66ec36ed3c5c3a4fb',1,'pl_search::Pred::engine'],['../classpl__search_1_1LoopBodyFactory.html#aef696239514a66eccb80ab193c46eff8',1,'pl_search::LoopBodyFactory::engine']]],
+  ['engine_2',['engine',['../classpl__search_1_1Engine.html',1,'pl_search::Engine'],['../classpl__search_1_1VarChoiceIterator.html#af557b6f1a2177f6ec6edebab0d7be2f7',1,'pl_search::VarChoiceIterator::engine'],['../classpl__search_1_1Pred.html#a3866f81bf27c42b66ec36ed3c5c3a4fb',1,'pl_search::Pred::engine'],['../classpl__search_1_1LoopBodyFactory.html#aef696239514a66eccb80ab193c46eff8',1,'pl_search::LoopBodyFactory::engine'],['../classpl__search_1_1Engine.html#a961cef69a59a6c81ff8e5965ba74ebfa',1,'pl_search::Engine::Engine()']]],
   ['engine_2ecpp_3',['engine.cpp',['../engine_8cpp.html',1,'']]],
   ['engine_2ehpp_4',['engine.hpp',['../engine_8hpp.html',1,'']]],
   ['env_5fentry_5',['env_entry',['../structpl__search_1_1env__entry.html',1,'pl_search']]],
